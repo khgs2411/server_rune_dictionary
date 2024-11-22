c7b07c8524e3b822b3c385e4cadd0583a423eac2
Liad Goren <liad@tigloo.io>
2024-11-22 15:44:44 +0200

    trying to resolve CORS

--- src/App.ts
+++ src/App.ts
@@ -12,7 +12,7 @@ const HEADERS = {
 	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
 	"Access-Control-Allow-Headers": "Content-Type, Authorization, x-requested-with",
 	"Access-Control-Allow-Credentials": "true",
-	Vary: "Origin",
+	// Vary: "Origin",
 };
 
 class App {
