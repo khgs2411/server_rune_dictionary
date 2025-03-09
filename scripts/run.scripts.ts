d7a5a65d87e92fc14ebb1d3387a274c1e5260ad8
Liad Goren <liad@tigloo.io>
2025-03-09 22:11:19 +0200

    .

--- scripts/run.scripts.ts
+++ scripts/run.scripts.ts
@@ -6,7 +6,8 @@ import * as auth from "./auth.scripts";
 export const run = async () => {
 	try {
 		// const res = await auth.run();
-		const res = await runes.run();
+		// const res = await runes.run();
+		const res = await mongo.run();
 		console.log(res);
 	} catch (e) {
 		console.error(e);
